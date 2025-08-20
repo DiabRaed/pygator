cb4659669bb3b2fa9bb23b6ca3be37c27a5858e2
Raed Diab <raed.diab@ufl.edu>
2025-08-20 14:12:28 -0400

    updated_tests

--- Tests/ABCD_focal_length.py
+++ Tests/ABCD_focal_length.py
@@ -2,7 +2,7 @@
 import pygator
 
 #test from JamMt #300 mm lens at 34cm with input beam at 0 of waist 370 µm
-fx,fy=gatorspy.calculate_focal_length(w0x_in=370e-6,w0y_in=370e-6,zx_in=40e-2,zy_in=34e-2,
+fx,fy=pygator.module.calculate_focal_length(w0x_in=370e-6,w0y_in=370e-6,zx_in=40e-2,zy_in=34e-2,
 w0x_out=273e-6,w0y_out=273e-6,zx_out=-32e-2,zy_out=-32e-2)
 print(fx,fy)
 
